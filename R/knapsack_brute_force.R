58a1b8eecfa601084e492c5967b09f5e6e057df2
M Cherangi <muma224466@gmail.com>
2023-10-12 16:29:37 +0200

    Update brute force
    Update brute force knapsack

--- R/knapsack_brute_force.R
+++ R/knapsack_brute_force.R
@@ -14,19 +14,38 @@
 #' @export knapsack_brute_force
 
 
-knapsack_brute_force <- function(x, W){
-
-  # ---V--- CHECK INPUT ---V---
-  # Check arg1 argument
-  stopifnot("argument \"x\" is not data.frame" = is.data.frame(x))
-
-  # Check arg2 argument
-  stopifnot("argument \"W\" is not numeric" = is.numeric(W))
-  # ---^--- CHECK INPUT ---^---
-
-  # Format and return output
-  output <- list("value" = 1,
-                 "elements" = c(1,2,3))
-  return(output)
+knapsack_brute_force <- function(x, W) {
+  # Check if x is a data.frame with the required structure
+  if (!is.data.frame(x) || !all(c("v", "w") %in% names(x)) || any(x$v <= 0) || any(x$w <= 0)) {
+    stop("Input 'x' must be a data.frame with 'v' and 'w' variables containing positive values.")
+  }
+  
+  n <- nrow(x)  # Number of items
+  max_value <- 0
+  selected_items <- numeric(0)
+  
+  # Generate all combinations of item indices using binary representation
+  for (i in 0:(2^n - 1)) {
+    combination <- as.integer(intToBits(i))[1:n]
+    total_value <- sum(x$v * combination)
+    total_weight <- sum(x$w * combination)
+    
+    if (total_weight <= W && total_value > max_value) {
+      max_value <- total_value
+      selected_items <- which(combination == 1)
+    }
+  }
+  
+  result <- list(value = max_value, elements = selected_items)
+  return(result)
 }
+
+# Example usage
+n <- 2000
+set.seed(42, kind = "Mersenne-Twister", normal.kind = "Inversion")  # For reproducibility
+knapsack_objects <- data.frame(w = sample(1:4000, size = n, replace = TRUE),
+                               v = runif(n, 0, 10000))
+knapsack_brute_force(x = knapsack_objects[1:8,], W = 3500)
+knapsack_brute_force(x = knapsack_objects[1:12,], W = 3500)
+knapsack_brute_force(x = knapsack_objects[1:8,], W = 2000)
 
