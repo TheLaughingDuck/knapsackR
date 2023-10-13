46d842803f5aba2cacb146b3dfe18d3e796f6022
Simon Jorstedt <jorstedtsimon@gmail.com>
2023-10-13 14:15:43 +0200

    Restored old changes manually
    Fixed the issues with function names that was introduced previously. Apparently had to do it manually to make sure we got it right.

--- tests/testthat/test-dynamic_knapsack.R
+++ tests/testthat/test-dynamic_knapsack.R
@@ -12,39 +12,39 @@ knapsack_objects <- data.frame(
 # Unit tests based on pre-made unit tests from the other two functions
 
 test_that("Correct object is returned", {
-  expect_silent(gk <- knapsack_dynamic(x = knapsack_objects[1:8,], W = 3500))
+  expect_silent(gk <- dynamic_knapsack(x = knapsack_objects[1:8,], W = 3500))
   expect_named(gk, c("value", "elements"))
 })
 
 test_that("functions rejects errounous input.", {
-  expect_error(knapsack_dynamic("hej", 3500))
-  expect_error(knapsack_dynamic(x = knapsack_objects[1:8,], W = -3500))
+  expect_error(dynamic_knapsack("hej", 3500))
+  expect_error(dynamic_knapsack(x = knapsack_objects[1:8,], W = -3500))
 })
 
 test_that("Function return correct results.", {
-  gk <- knapsack_dynamic(x = knapsack_objects[1:8,], W = 3500)
+  gk <- dynamic_knapsack(x = knapsack_objects[1:8,], W = 3500)
   expect_equal(round(gk$value), 16770)
   expect_true(all(round(gk$elements) %in% c(5, 8)))
 
-  gk <- knapsack_dynamic(x = knapsack_objects[1:12,], W = 3500)
+  gk <- dynamic_knapsack(x = knapsack_objects[1:12,], W = 3500)
   expect_equal(round(gk$value), 16770)
   expect_true(all(round(gk$elements) %in% c(5, 8)))
 
-  gk <- knapsack_dynamic(x = knapsack_objects[1:8,], W = 2000)
+  gk <- dynamic_knapsack(x = knapsack_objects[1:8,], W = 2000)
   expect_equal(round(gk$value), 15428)
   expect_true(all(round(gk$elements) %in% c(3, 8)))
 
-  gk <- knapsack_dynamic(x = knapsack_objects[1:12,], W = 2000)
+  gk <- dynamic_knapsack(x = knapsack_objects[1:12,], W = 2000)
   expect_equal(round(gk$value), 15428)
   expect_true(all(round(gk$elements) %in% c(3, 8)))
 
-  st <- system.time(gk <- knapsack_dynamic(x = knapsack_objects[1:16,], W = 2000))
+  st <- system.time(gk <- dynamic_knapsack(x = knapsack_objects[1:16,], W = 2000))
   expect_true(as.numeric(st)[2] <= 0.01)
 
-  # gk <- knapsack_dynamic(x = knapsack_objects[1:800,], W = 3500)
+  # gk <- dynamic_knapsack(x = knapsack_objects[1:800,], W = 3500)
   # expect_equal(round(gk$value), 192647)
   #
-  # gk <- knapsack_dynamic(x = knapsack_objects[1:1200,], W = 3500)
+  # gk <- dynamic_knapsack(x = knapsack_objects[1:1200,], W = 3500)
   # expect_equal(round(gk$value), 270290)
 })
 # --------------------------------------------------------------------
@@ -52,13 +52,13 @@ test_that("Function return correct results.", {
 # ------------------------------------
 # Unit tests made up by the developers
 
-test_that("knapsack_dynamic returns correct output",{
-  output <- expect_error(knapsack_dynamic(iris, 2))
+test_that("dynamic_knapsack returns correct output",{
+  output <- expect_error(dynamic_knapsack(iris, 2))
   #expect_true(is.null(output$value) == FALSE & is.null(output$elements) == FALSE)
 })
 
-test_that("knapsack_dynamic wikipedia example works",{
-  output <- knapsack_dynamic(data.frame(v=c(5,4,3,2), w=c(4,3,2,1)), W = 6)
+test_that("dynamic_knapsack wikipedia example works",{
+  output <- dynamic_knapsack(data.frame(v=c(5,4,3,2), w=c(4,3,2,1)), W = 6)
 
   expect_true(all(output$value == 9 & output$elements %in% c(4,3,2)))
 })
