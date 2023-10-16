c56d4148bba96647c59bd7230fbbe1410a589d18
M Cherangi <muma224466@gmail.com>
2023-10-16 20:45:35 +0200

    updated brute force with parallel

--- R/brute_force_knapsack.R
+++ R/brute_force_knapsack.R
@@ -12,35 +12,82 @@
 #' @source [The wikipedia article on the Knapsack problem](https://en.wikipedia.org/wiki/Knapsack_problem)
 #'
 #' @export brute_force_knapsack
+#' @import parallel
 
 
-brute_force_knapsack <- function(x, W) {
-  # ---V--- CHECK INPUT ---V---
+
+# Define the compute_knapsack function
+compute_knapsack <- function(i, x, W) {
+  n <- nrow(x)
+  max_value <- 0
+  selected_items <- numeric(0)
+  
+  combination <- as.integer(intToBits(i))[1:n]
+  total_value <- sum(x$v * combination)
+  total_weight <- sum(x$w * combination)
+  
+  if (total_weight <= W && total_value > max_value) {
+    max_value <- total_value
+    selected_items <- which(combination == 1)
+  }
+  
+  return(list(value = max_value, elements = selected_items))
+}
+
+brute_force_knapsack <- function(x, W, parallel = FALSE) {
   # Check if x is a data.frame with the required structure
-  if (!is.data.frame(x) || !all(c("v", "w") %in% names(x)) || any(x$v <= 0) || any(x$w <= 0)) {
+  if (!is.data.frame(x) && !all(c("v", "w") %in% names(x)) && any(x$v <= 0) && any(x$w <= 0)) {
     stop("Input 'x' must be a data.frame with 'v' and 'w' variables containing positive values.")
   }
-
-  stopifnot("argument \"W\" is not positive" = W > 0)
-  # ---^--- CHECK INPUT ---^---
-
+  
   n <- nrow(x)  # Number of items
   max_value <- 0
   selected_items <- numeric(0)
-
+  
   # Generate all combinations of item indices using binary representation
-  for (i in 0:(2^n - 1)) {
-    combination <- as.integer(intToBits(i))[1:n]
-    total_value <- sum(x$v * combination)
-    total_weight <- sum(x$w * combination)
-
-    if (total_weight <= W && total_value > max_value) {
-      max_value <- total_value
-      selected_items <- which(combination == 1)
+  combinations <- 0:(2^n - 1)
+  
+  if (parallel) {
+    # Parallelize using multiple cores
+    num_cores <- detectCores()
+    clusters <- makeCluster(num_cores)
+    clusterExport(clusters, "intToBits")
+    
+    result <- parLapply(clusters, combinations, function(i) {
+      compute_knapsack <- function(i, x, W) {  # Define the function within the parallel worker
+        n <- nrow(x)
+        max_value <- 0
+        selected_items <- numeric(0)
+        
+        combination <- as.integer(intToBits(i))[1:n]
+        total_value <- sum(x$v * combination)
+        total_weight <- sum(x$w * combination)
+        
+        if (total_weight <= W && total_value > max_value) {
+          max_value <- total_value
+          selected_items <- which(combination == 1)
+        }
+        
+        return(list(value = max_value, elements = selected_items))
+      }
+      
+      return(compute_knapsack(i, x, W))
+    })
+    
+    stopCluster(clusters)
+  } else {
+    # Non-parallel version
+    for (i in combinations) {
+      knapsack_result <- compute_knapsack(i, x, W)
+      if (knapsack_result$value > max_value) {
+        max_value <- knapsack_result$value
+        selected_items <- knapsack_result$elements
+      }
     }
   }
-
+  
   result <- list(value = max_value, elements = selected_items)
   return(result)
 }
+
 
